3476292ef64c2c0dc75d6491041000bd9dd2f0b4
shufang <shufangreal@126.com>
2018-12-05 18:50:56 +0800

    "hellofork"

---
+++ Fork/Forkman.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Jerevon on 2018/12/5.
+ */
+public class Forkman {
+
+    public static void main(String[] args) {
+        System.out.println("if  you reallly want just show me your body");
+        System.out.println("if  you reallly want just show me your body");
+        System.out.println("if  you reallly want just show me your body");
+        System.out.println("if  you reallly want just show me your body");
+        System.out.println("if  you reallly want just show me your body");
+        System.out.println("if  you reallly want just show me your body");
+        System.out.println("if  you reallly want just show me your body");
+        System.out.println("if  you reallly want just show me your body");
+        System.out.println("if  you reallly want just show me your body");
+    }
+}
